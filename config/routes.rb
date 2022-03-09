189312c8b8f5b721eecedc36b80e14c8785ceb7b
Felipe Frizzo <fernando.stopasola@gmail.com>
2022-03-09 11:14:55 -0300

    Add devise and create Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, :path => 'user' do
+    get '/user/sign_out' => 'devise/sessions#destroy' 
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "home/index"  
-  root to: "home#index"  
+  # get "home/index"  
+  root "home#index"  
   
 end
